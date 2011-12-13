19700db9b76f9e2370ba5fd398e1f45dc0623e7d
Soumya Deb <deb.webdev@gmail.com>
2011-12-13 14:16:32 +0530

    Introducing WorkFlow Module

---
+++ workflow.js
@@ -0,0 +1,46 @@
+/*
+ * The contact.js extends the Boxcite object & provides
+ * all the contact-specific properties & methods.
+ *
+ * @copyright mQuotient Solutions Limited
+ * @developer Soumya Deb [DebWebDev]
+ *
+ */
+
+var workflow = new Boxcite;
+
+workflow.prefix = 'mq.boxcite.workflow.';
+workflow.createTransaction = function ( data ) {
+	try {
+		var method = "createTransaction";
+		response = this.sendRequest( method, data );
+		return response;
+	}
+	catch ( error ) {
+		error = "Error : " + error;
+		throw error;
+	}
+}
+
+workflow.readTransaction = function ( data ) {
+	try {
+		var method = "readTransaction";
+		response = this.sendRequest( method, data );
+		return response;
+	}
+	catch ( error ) {
+		error = "Error : " + error;
+		throw error;
+	}
+}
+workflow.findTransaction = function ( data ) {
+	try {
+		var method = "findTransaction";
+		response = this.sendRequest( method, data );
+		return response;
+	}
+	catch ( error ) {
+		error = "Error : " + error;
+		throw error;
+	}
+}
